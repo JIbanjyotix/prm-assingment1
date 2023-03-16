491d0f6a8b18343e29bede28dcca84ceafb60d5a
JIbanjyotix <115848299+JIbanjyotix@users.noreply.github.com>
2023-03-16 20:29:06 +0530

    Add files via upload

---
+++ shorted list.py
@@ -0,0 +1,34 @@
+# Python Candidates - Question 1
+
+# You will have a number of elements and in the next n lines element of a list. You have to create a list from the given strings. You have to sort the list based on 2nd last character of a string.
+
+# For example: given list = ['great','hello','hiyo','abc'] so your output_dictionary should be ['great', 'abc', 'hello','hiyo']
+
+# Input Format:
+
+# At first-line it will have an integer (number of elements inside a list). In the second line, it will have a string.
+
+# Output Format:
+
+# A single line containing a sorted list.
+
+# solution 1
+n = int(input())
+a = [input() for i in range(n)]
+a.sort(key=lambda x:x[-2])
+print(a)
+
+# solution 2
+def sl(a):
+    return a[-2]
+n = int(input())
+a = [input() for i in range(n)]
+a_sort = sorted(a,key=sl)
+print(a_sort)
+
+# solution 3
+n = int(input())
+a = [input() for i in range(n)]
+for i in a:
+    a.sort(key = lambda i:i[-2])
+print(a)
